a22e03282be2c03fd13ed2db14e578066d4a3091
ahmad17-fe <ahmads1704@gmail.com>
2021-04-07 18:48:51 +0700

    update container

--- View/Style/Style.js
+++ View/Style/Style.js
@@ -1,16 +1,16 @@
-import * as React from 'react';
+import * as React from 'react'
 
-import { StyleSheet,Dimensions } from 'react-native';
-const { width: WIDTH} = Dimensions.get('window');
-const windowHeight = Dimensions.get('window').height;
+import { StyleSheet, Dimensions } from 'react-native'
+const { width: WIDTH } = Dimensions.get('window')
+const windowHeight = Dimensions.get('window').height
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
+  container: {
+    width: WIDTH,
+    height: windowHeight,
+    backgroundColor: '#05445E',
+    paddingHorizontal: 20,
+  },
+})
 
-  export default styles
+export default styles
