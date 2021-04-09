6fb539bd82f17008782785af50fb8beddd11e0bf
Ahmad <ahmads1704@gmail.com>
2021-04-10 00:23:09 +0700

    Refactor Beranda

--- View/Beranda/Beranda.js
+++ View/Beranda/Beranda.js
@@ -1,57 +1,18 @@
 import * as React from 'react'
-import {
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  StatusBar,
-  FlatList,
-  ImageBackground,
-} from 'react-native'
+import { SafeAreaView, View, FlatList } from 'react-native'
 
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import moment from 'moment'
-import Style from './../Style/Style'
+import Style, { ijo, oren, putih } from './../Style/Style'
 import Project from '../../components/Project'
 import DATA from './dataDummy'
+import HeaderComponent from './HeaderComponent'
 
 function renderProject({ item }) {
   return (
     <View style={Style.wrapper}>
       <Project data={item} />
     </View>
-  )
-}
-
-function headerComponent() {
-  return (
-    <>
-      <View style={[styles.topBar, Style.wrapper]}>
-        <View style={styles.profile}>
-          <View style={styles.profile_img}>
-            <Image
-              style={{ width: '100%', height: '100%' }}
-              source={require('../../assets/person-female.jpg')}
-            />
-          </View>
-          <View>
-            <Text style={Style.textNormalWhite}>Selamat Datang</Text>
-            <Text style={[Style.textBold]}>Clara Laudia</Text>
-          </View>
-        </View>
-        <View style={styles.logo}>
-          <ImageBackground
-            style={{ width: '100%', height: '100%' }}
-            source={require('../../assets/poltek.png')}
-          />
-        </View>
-      </View>
-
-      <Text style={[Style.textBold, Style.wrapper, { marginBottom: 20 }]}>
-        Status Proyek
-      </Text>
-    </>
   )
 }
 
@@ -65,13 +26,12 @@ class Beranda extends React.Component {
   }
 
   render() {
-    const { navigation } = this.props
     return (
       <View style={Style.container}>
         <SafeAreaView>
           <FlatList
             data={DATA}
-            ListHeaderComponent={headerComponent}
+            ListHeaderComponent={HeaderComponent}
             renderItem={renderProject}
             keyExtractor={(item) => item.id}
             showsHorizontalScrollIndicator={false}
@@ -83,29 +43,4 @@ class Beranda extends React.Component {
   }
 }
 
-const styles = StyleSheet.create({
-  topBar: {
-    paddingVertical: StatusBar.currentHeight + 5 || 0,
-    overflow: 'hidden',
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  profile: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  profile_img: {
-    width: 50,
-    height: 50,
-    overflow: 'hidden',
-    borderRadius: 50,
-    marginRight: 10,
-  },
-  logo: {
-    width: 70,
-    height: 46,
-  },
-})
 export default Beranda
