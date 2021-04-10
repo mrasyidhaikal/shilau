054274fe853dc440ba46cdbfbd8b43c5faa8f479
Programmer Kuadran <programmer.kuadran@gmail.com>
2021-04-10 14:43:27 +0700

    Usulan Navigation

--- View/Beranda/Beranda.js
+++ View/Beranda/Beranda.js
@@ -1,57 +1,54 @@
-import * as React from 'react';
-import {SafeAreaView, StyleSheet, Text, View,Image,Button, Dimensions,TouchableOpacity,TextInput, StatusBar,ScrollView, RefreshControl } from 'react-native';
+import * as React from 'react'
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Button,
+  Dimensions,
+  TouchableOpacity,
+  TextInput,
+  StatusBar,
+  ScrollView,
+  RefreshControl,
+} from 'react-native'
 
-
-import Icon from 'react-native-vector-icons/Ionicons';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import moment from 'moment';
+import Icon from 'react-native-vector-icons/Ionicons'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import moment from 'moment'
 import Style from './../Style/Style'
 
+class Beranda extends React.Component {
+  constructor() {
+    super()
 
-class Beranda extends React.Component{
-    
-    constructor() {
-        super()
-       
-
-
-        this.state = {
-       
-            refreshing: false,
-           
-        }
+    this.state = {
+      refreshing: false,
     }
+  }
 
-
-
-    render(){
-    
-      const { navigation } = this.props;
-    return(
-    
-        <View style={Style.container}>
+  render() {
+    const { navigation } = this.props
+    return (
+      <View style={Style.container}>
         <SafeAreaView>
-            <ScrollView>
-            
-
-        
-     
-
-            </ScrollView>
-    
-        </SafeAreaView>  
+          <ScrollView>
+            <TouchableOpacity
+              style={Style.buttonGhost}
+              onPress={() =>
+                navigation.navigate('AddUsulanStack', {
+                  screen: 'usulanPerusahaan',
+                })
+              }
+            >
+              <Text style={Style.textBold}></Text>
+            </TouchableOpacity>
+          </ScrollView>
+        </SafeAreaView>
       </View>
-      
     )
-   
-    }
-
+  }
 }
 
-  export default Beranda
-
-
-
-
-
-  
+export default Beranda
