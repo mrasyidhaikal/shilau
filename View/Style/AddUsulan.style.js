9e250299262a75c75696b4f452fab227210a7420
Ikhwan A Latif <ikhwanal235@gmail.com>
2021-04-11 15:17:28 +0700

    li'l change

--- View/Style/AddUsulan.style.js
+++ View/Style/AddUsulan.style.js
@@ -4,16 +4,10 @@ export default StyleSheet.create({
     AdditionalInputStyle: {
         marginBottom: 20,
         marginTop: 20,
-        paddingLeft: 20,
     },
     FormTextInputView: {
         justifyContent: 'center',
         alignItems: 'center',
-    },
-    contentContainer: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
     },
     AdditionalButton: {
         alignSelf: 'center',
